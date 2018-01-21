dca92fdaab7633bf2e5c55175a93c1f9c59c67be
aqent <ryan-hop@hotmail.com>
2018-01-20 19:30:31 -0600

    Initial commit

---
+++ app/src/main/java/com/example/ryan/triciahopson/ALovingMother.java
@@ -0,0 +1,13 @@
+package com.example.ryan.triciahopson;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ALovingMother extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aloving_mother);
+    }
+}
